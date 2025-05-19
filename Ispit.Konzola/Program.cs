d0d8782399f3566d2c8bfc1426facdfbb93c5bb9
Kresimir Kovacevic <kk53407@fer.hr>
2025-05-19 21:01:17 +0200

    Third commit

---
+++ Ispit.Konzola/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ispit.Model;
+
+namespace Ispit.Konzola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            List<Ucenik> ucenici = new List<Ucenik>();
+            string[] unosi = { };
+            ushort maxDuljina;
+            string rezultat;
+            string okvir;
+            Console.WriteLine(double.Parse("2.45", NumberStyles.Any, CultureInfo.InvariantCulture));
+            Console.WriteLine("Početak rada...\nADD = dodaj ucenika\nPRINT = ispis svih ucenika\nREMOVE = ukloni ucenika\nCLEAR = ukloni sve ucenike\nQUIT = izlaz iz programa");
+            do {
+                Console.Write("\nUnesite komandu: ");
+                string unos = Console.ReadLine();
+                unos = unos.Trim().ToLower();
+                if (unos == "quit")
+                {
+                    break;
+                }
+                switch (unos)
+                {
+                    case "add":
+                        while (true)
+                        {
+                            Console.Write("\nUnesite podatke za ucenika ('ime, prezime, YYYY-MM-DD, a.bc' ili QUIT za odustajanje): ");
+                            unos = Console.ReadLine();
+                            if (unos.Trim().ToLower() == "quit")
+                            {
+                                break;
+                            }
+                            unosi = unos.Split(',');
+                            if (unosi.Length != 4)
+                            {
+                                Console.WriteLine("Neispravan broj parametara!");
+                                continue;
+                            }
+                            for (ushort i = 0; i < unosi.Length; i++)
+                            {
+                                unosi[i] = unosi[i].Trim();
+                            }
+                            if (Ucenik.ProvjeraUnosa(unosi[0], unosi[1], unosi[2], unosi[3]))
+                            {
+                                break;
+                            }
+                        }
+                        if(unos.ToLower().Trim() == "quit")
+                        {
+                            break;
+                        }
+                        ucenici.Add(new Ucenik(unosi[0], unosi[1], DateTime.Parse(unosi[2]), Double.Parse(unosi[3], NumberStyles.Any, CultureInfo.InvariantCulture)));
+                        Console.WriteLine("Dodan ucenik!");
+                        if (ucenici.Count == 3)
+                        {
+                            maxDuljina = 0;
+                            rezultat = "";
+                            for( ushort i = 0; i < ucenici.Count; i++)
+                            {
+                                string meduRez = i + 1 + ".\t" + ucenici[i].ToString()+"\n";
+                                if(meduRez.Length > maxDuljina)
+                                {
+                                    maxDuljina = (ushort)meduRez.Length;
+                                }
+                                rezultat += meduRez;
+                            }
+                            okvir = ""; 
+                            for(ushort i = 0;i < maxDuljina; i++)
+                            {
+                                okvir += "-";
+                            }
+                            Console.WriteLine(okvir + "\n" + rezultat + okvir);
+                        }
+                        break;
+                    case "print":
+                        if(ucenici.Count == 0)
+                        {
+                            Console.WriteLine("EMPTY!");
+                            break;
+                        }
+                        maxDuljina = 0;
+                        rezultat = "";
+                        for (ushort i = 0; i < ucenici.Count; i++)
+                        {
+                            string meduRez = i + 1 + ".\t" + ucenici[i].ToString() + "\n";
+                            if (meduRez.Length > maxDuljina)
+                            {
+                                maxDuljina = (ushort)meduRez.Length;
+                            }
+                            rezultat += meduRez;
+                        }
+                        okvir = "";
+                        for (ushort i = 0; i < maxDuljina; i++)
+                        {
+                            okvir += "-";
+                        }
+                        Console.WriteLine(okvir + "\n" + rezultat + okvir);
+                        break;
+                    case "remove":
+                        if (ucenici.Count == 0)
+                        {
+                            Console.WriteLine("Nema ucenika za ukoniti!");
+                            break;
+                        }
+                        while(true){
+                            Console.Write("\nUnesite indeks ucenika kojeg zelite ukloniti (ili QUIT za odustajanje): ");
+                            unos = Console.ReadLine();
+                            if (unos.Trim().ToLower() == "quit")
+                            {
+                                break;
+                            }
+                            int indeks;
+                            if (int.TryParse(unos, out indeks))
+                            {
+                                if (indeks < 1 || indeks > ucenici.Count)
+                                {
+                                    Console.WriteLine("Indeks izvan dosega!");
+                                }
+                                else
+                                {
+                                    ucenici.RemoveAt(indeks-1);
+                                    Console.WriteLine($"Ucenik {indeks} uspješno uklonjen!");
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Indeks se nije uspio parsirati!");
+                            }
+                        }
+                        break;
+                    case "clear":
+                        ucenici.Clear();
+                        Console.WriteLine("Popis učenika uspješno obrisan!");
+                        break;
+                }
+            } while (true);
+            Console.WriteLine("\nKraj rada...");
+        }
+    }
+}
---
+++ Ispit.Model/Ucenik.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Model
+{
+    public class Ucenik
+    {
+        #region Svojstva
+        protected string _ime;
+        protected string _prezime;
+        protected DateTime _datumRodjenja;
+        protected double _prosjek;
+        #endregion
+
+        #region Getteri i Setteri
+        public string Ime
+        {
+            get { return _ime; }
+            set
+            {
+                if (value == null || value == "")
+                {
+                    throw new ArgumentException("Ime ne može biti prazno!");
+                }
+                _ime = value;
+            }
+        }
+
+        public string Prezime
+        {
+            get { return _prezime; }
+            set
+            {
+                if (value == null || value == "")
+                {
+                    throw new ArgumentException("Prezime ne može biti prazno!");
+                }
+                _prezime = value;
+            }
+        }
+
+        public DateTime DatumRodjenja
+        {
+            get { return _datumRodjenja; }
+            set
+            {
+                if (value > DateTime.Now)
+                {
+                    throw new ArgumentException("Datum rodjenja mora biti u proslosti!");
+                }
+                _datumRodjenja = value;
+            }
+        }
+
+        public double Prosjek
+        {
+            get { return _prosjek; }
+            set
+            {
+                if (!(value <= 5.00 && value >= 1.00))
+                {
+                    throw new ArgumentException("Prosjek mora biti od 1.0 do 5.0!");
+                }
+                _prosjek = value;
+            }
+        }
+        #endregion
+
+        #region Konstruktori
+        public Ucenik(string ime, string prezime, DateTime datumRodjenja, double prosjek)
+        {
+            Ime = ime;
+            Prezime = prezime;
+            DatumRodjenja = datumRodjenja;
+            Prosjek = prosjek;
+        }
+        #endregion
+
+        #region Metode
+        public int Starost()
+        {
+            int godine = DateTime.Now.Year - DatumRodjenja.Year;
+            if (DateTime.Now.Month <= DatumRodjenja.Month && DateTime.Now.Day < DatumRodjenja.Day)
+            {
+                godine--;
+            }
+            return godine;
+        }
+
+        public string IspisProsjeka(bool print = true)
+        {
+            string rezultat = "Prosjek izvan dosega!";
+            if (Prosjek >= 1.0 && Prosjek < 1.5)
+            {
+                rezultat = "medovoljan";
+            }
+            else if (Prosjek >= 1.5 && Prosjek < 2.5)
+            {
+                rezultat = "dovoljan";
+            }
+            else if (Prosjek >= 2.5 && Prosjek < 3.5)
+            {
+                rezultat = "dobar";
+            }
+            else if (Prosjek >= 3.5 && Prosjek < 4.5)
+            {
+                rezultat = "vrlo dobar";
+            }
+            else if (Prosjek >= 4.5 && Prosjek < 5.0)
+            {
+                rezultat = "odličan";
+            }
+            if (print)
+            {
+                Console.WriteLine(rezultat);
+            }
+            return rezultat;
+        }
+
+        override public string ToString()
+        {
+            return $"[{Ime} {Prezime}, {DatumRodjenja} ({this.Starost()} godina), prosjek: {Prosjek} ({this.IspisProsjeka(false)})]";
+        }
+
+        public static bool ProvjeraUnosa(string ime, string prezime, string datumRodjenja, string prosjek)
+        {
+            bool prihvatljivo = true;
+            DateTime parsiranDatumRodjenja;
+            double parsiranProsjek;
+            if (ime == null || ime == "")
+            {
+                Console.WriteLine("Ime ne može biti prazno!");
+                prihvatljivo = false;
+            }
+            if (prezime == null || prezime == "")
+            {
+                Console.WriteLine("Prezime ne može biti prazno!");
+                prihvatljivo = false;
+            }
+            if (DateTime.TryParse(datumRodjenja, out parsiranDatumRodjenja))
+            {
+                if (parsiranDatumRodjenja > DateTime.Now)
+                {
+                    Console.WriteLine("Datum rodjenja mora biti u proslosti!");
+                    prihvatljivo = false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Datum rođenja se ne može parsirati!");
+                prihvatljivo = false;
+            }
+            if (Double.TryParse(prosjek, NumberStyles.Any, CultureInfo.InvariantCulture, out parsiranProsjek))
+            {
+                if (!(parsiranProsjek <= 5.00 && parsiranProsjek >= 1.00))
+                {
+                    Console.WriteLine("Prosjek mora biti od 1.0 do 5.0!");
+                    prihvatljivo = false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Prosjek se ne može parsirati!");
+                prihvatljivo = false;
+            }
+            return prihvatljivo;
+        }
+        #endregion
+    }
+}
